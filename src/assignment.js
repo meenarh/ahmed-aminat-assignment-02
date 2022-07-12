88bbbfbfa2e9df715ab75cd8d113eda9b5ee56c6
Dodo <oahmedaminat@gmail.com>
2022-07-12 13:22:47 +0000

    updated assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -23,6 +23,7 @@ function sumOfNumbers(arrayOfNumbers) {
 }
 
 console.log(sumOfNumbers([3, 7, 9, 5]));
+
 assignment.sumOfNumbers = sumOfNumbers;
 
 /**
@@ -45,6 +46,7 @@ function countEvenNumbers(arrayOfNumbers) {
     return count;
 }
 console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]));
+
 assignment.countEvenNumbers = countEvenNumbers;
 
 
